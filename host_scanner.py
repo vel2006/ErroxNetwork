d74730302d44a3044f539549d92041fd034cdc79
That1EthicalHacker <116861198+vel2006@users.noreply.github.com>
2024-12-24 12:04:46 -0600

    Update host_scanner.py
    Updating the script to include multiprocessing and other neat little things that make the process of scanning the device faster and easier

--- host_scanner.py
+++ host_scanner.py
@@ -1,4 +1,6 @@
-from kamene.all import Ether, ARP, IP, ICMP, conf, sr, srp
+from kamene.all import Ether, ARP, IP, ICMP, conf, sr, srp, sr1, TCP, UDP
+from multiprocessing import Manager, Process
+import random
 class PortScannerError(Exception):
     def __init__(self, error_message):
         super().__init__(error_message)
@@ -19,16 +21,21 @@ class PortScanner():
         888, 898, 900, 901, 902, 903, 911, 912, 981, 987, 990, 992, 993, 995, 999, 1000, 6069, 6070)
     def syn_scan_port(self, destination_ip:str, destination_port:int, timeout_length:int):
         """
-            \'syn_scan_port\' will do a scan on target device port. Returns True if the device responded, False if the device did not respond
+            \'syn_scan_port\' will do a scan on target device port.
             Below are the arguments explained:
                 \'destination_ip\':str is an argument that will set the target IPv4 address to scan
                 \'destination_port\':int is an argument that will set the target port to scan
                 \'timeout_length\':int is an argument that will set the timeout_length for the TCP connection
         """
-        answered, unanswered = sr1(IP(dst=destination_ip, ttl=64)/TCP(dport=destination_port, flags='s'), timeout=timeout_length)
-        if answered:
-            return True
-        return False
+        response, noresponse = sr(IP(dst=destination_ip)/TCP(dport=destination_port, flags='S', options=[('Timestamp',(random.randint(0, 10),random.randint(10, 20)))]), timeout=10, verbose=0)
+        if noresponse:
+            return (False, "Port is closed", destination_port)
+        match response[0][1].flags:
+            case 2:
+                return (True, "Port is open", destination_port)
+            case _:
+                return (True, "Port state is unknown", destination_port)
+        return PortScannerError(f"IP address {destination_ip} did not respond on port {destination_port}")
     def ack_scan_port(self, destination_ip:str, destination_port:int, timeout_length:int):
         """
             \'udp_scan_port\' will do a scan on target device port. Returns True if the device responded, False if the device did not respond
@@ -37,22 +44,15 @@ class PortScanner():
                 \'destination_port\':int is an argument that will set the target port to scan
                 \'timeout_length\':int is an argument that will set the timeout_length for the TCP connection
         """
-        answered, unanswered = sr1(IP(dst=destination_ip, ttl=64)/TCP(dport=destination_port, flags='a'), timeout=timeout_length)
-        if answered:
-            return True
-        return False
-    def udp_scan_port(self, destination_ip:str, destination_port:int, timeout_length:int):
-        """
-            \'udp_scan_port\' will do a scan on target device port. Returns True if the device responded, False if the device did not respond
-            Below are the arguments explained:
-                \'destination_ip\':str is an argument that will set the target IPv4 address to scan
-                \'destination_port\':int is an argument that will set the target port to scan
-                \'timeout_length\':int is an argument that will set the timeout_length for the UDP connection
-        """
-        answered, unanswered = sr1(IP(dst=destination_ip, ttl=64)/UDP(dport=destination_port), timeout=timeout_length)
-        if answered:
-            return True
-        return False
+        response, noresponse = sr(IP(dst=destination_ip)/TCP(dport=destination_port, flags='A', options=[('Timestamp',(random.randint(0, 10),random.randint(10, 20)))]), timeout=10, verbose=0)
+        if noresponse:
+            return (False, "Port is closed")
+        match response[0][1].flags:
+            case 2:
+                return (True, "Port is open", destination_port)
+            case _:
+                return (True, "Port state is unknown", destination_port)
+        return PortScannerError(f"IP address {destination_ip} did not respond on port {destination_port}")
     def set_process_limit(self, new_process_limit:int):
         """
             Sets the process limit for managed scans, it will not be updated inside of currently running scans.
@@ -67,22 +67,20 @@ class PortScanner():
             'MED': self.MED_LIST
         }
         return return_dict
-    def managed_scan_port(self, destination_ip:str, tcp_connection:bool, connection_type:str, ports_to_scan:tuple, return_dictionary:dict, process_id:int):
+    def managed_scan_port(self, destination_ip:str, connection_type:str, ports_to_scan:tuple, return_dictionary:dict, process_id:int):
         """
             \'managed_scan_port\' is not to be called. It is a method used by \'managed_tcp_scan_ports\' and \'managed_udp_scan_ports\'.
             Not joking. Dont call this.
         """
         responded_ports = []
-        if tcp_connection is True:
-            for port in ports_to_scan:
-                answered, unanswered = sr1(IP(dst=destination_ip, ttl=64)/TCP(dport=port, flags=connection_type), timeout=5)
-                if answered:
-                    responded_ports.append(port)
-        else:
-            for port in ports_to_scan:
-                answered, unanswered = sr1(IP(dst=destination_ip, ttl=64)/UDP(dport=port), timeout=5)
-                if answered:
-                    responded_ports.append(port)
+        for port in ports_to_scan:
+            response, noresponse = sr(IP(dst=destination_ip)/TCP(dport=port, flags=connection_type, options=[('Timestamp',(random.randint(0, 10),random.randint(10, 20)))]), timeout=10, verbose=0)
+            if response:
+                match response[0][1].flags:
+                    case 2:
+                        responded_ports.append((True, "Port is open", port))
+                    case _:
+                        responded_ports.append((True, "Port is open", port))
         return_dictionary[process_id] = responded_ports
     def managed_tcp_scan_ports(self, target_ip:str, use_custom_port:bool, ports_to_scan:int, syn_scan:bool, custom_list:tuple):
         """
@@ -107,83 +105,62 @@ class PortScanner():
         return_dictionary = process_manager.dict()
         scan_processes = []
         last_ports_count = 0
-        if syn_scan is True:
-            match ports_to_scan:
-                case 0:
-                    ports_per_process = int(len(self.SML_LIST)/self.MAX_PROCESSES)
-                    for process_id in range(1, self.MAX_PROCESSES):
-                        process = Process.run(target=managed_scan_port, args=(target_ip, True, 's', self.SML_LIST[last_ports_count:(last_ports_count + ports_per_process)], return_dictionary, process_id))
-                        scan_processes.append(process)
-                case 1:
-                    ports_per_process = int(len(self.MED_LIST)/self.MAX_PROCESSES)
-                    for process_id in range(1, self.MAX_PROCESSES):
-                        process = Process.run(target=managed_scan_port, args=(target_ip, True, 's', self.MED_LIST[last_ports_count:(last_ports_count + ports_per_process)], return_dictionary, process_id))
-                        scan_processes.append(process)
-                case _:
-                    raise PortScannerError(f"Incompatable input scan type \'{ports_to_scan}\'!")
-        else:
-            match ports_to_scan:
-                case 0:
-                    ports_per_process = int(len(self.SML_LIST)/self.MAX_PROCESSES)
-                    for process_id in range(1, self.MAX_PROCESSES):
-                        process = Process.run(target=managed_scan_port, args=(target_ip, True, 'a', self.SML_LIST[last_ports_count:(last_ports_count + ports_per_process)], return_dictionary, process_id))
-                        scan_processes.append(process)
-                case 1:
-                    ports_per_process = int(len(self.MED_LIST)/self.MAX_PROCESSES)
-                    for process_id in range(1, self.MAX_PROCESSES):
-                        process = Process.run(target=managed_scan_port, args=(target_ip, True, 'a', self.MED_LIST[last_ports_count:(last_ports_count + ports_per_process)], return_dictionary, process_id))
-                        scan_processes.append(process)
-                case _:
-                    raise PortScannerError(f"Incompatable input scan type \'{ports_to_scan}\'!")
-        for process in scan_processes:
-            process.join()
-        for key in scan_processes.keys():
-            for port in scan_processes[key]:
-                open_ports.append(port)
-        return open_ports
-    def managed_udp_scan_ports(self, target_ip:str, use_custom_port:bool, ports_to_scan:int, custom_list:tuple):
-        """
-            \'managed_udp_scan_ports\' is designed to automate the scanning of ports listed inside of \'list_port_lists()\'.
-            It works by creating processes with evenly distrobuted ports that will be automatically scanned within each process.
-            Below the arguments are explained:
-                \'target_ip\':str is the target IPv4 address that you wish to scan
-                \'use_custom_port\':bool is a bool for scanning a custom list, if True it will pull the passed tuple in the \'custom_list\' argument
-                \'custom_list\':tuple is a variable that contains a custom tuple of ports that will be scanned
-                \'ports_to_scan\':int is a variable that will dictate the hard-coded list of ports that will be scanned (can only be 0 or 1)
-            Below are example usages:
-                (using a hard-coded list)
-                managed_udp_scan_ports("192.168.1.254", False, 0, None)
-                managed_udp_scan_ports(False, 1, None)
-                (using a custom list)
-                managed_udp_scan_ports("192.168.1.254", True, None, tuple([1, 4, 12, 18, 20, 80, 443, 2020, 8080, 1000]))
-        """
-        open_ports = []
-        process_manager = Manager()
-        return_dictionary = process_manager.dict()
-        scan_processes = []
-        last_ports_count = 0
         if use_custom_port is True:
             ports_per_process = int(len(custom_list)/self.MAX_PROCESSES)
-            for process_id in range(1, self.MAX_PROCESSES):
-                process = Process.run(target=managed_scan_port, args=(target_ip, False, '0', custom_list[last_ports_count:(last_ports_count + ports_per_process)], return_dictionary, process_id))
-                scan_processes.append(process)
+            if syn_scan is True:
+                for process_id in range(1, self.MAX_PROCESSES):
+                    process = Process(target=PortScanner().managed_scan_port, args=(target_ip, 'S', custom_list[last_ports_count:(last_ports_count + ports_per_process)], return_dictionary, process_id))
+                    process.start()
+                    scan_processes.append(process)
+                    last_ports_count += ports_per_process
+            else:
+                for process_id in range(1, self.MAX_PROCESSES):
+                    process = Process(target=PortScanner().managed_scan_port, args=(target_ip, 'A', custom_list[last_ports_count:(last_ports_count + ports_per_process)], return_dictionary, process_id))
+                    process.start()
+                    scan_processes.append(process)
+                    last_ports_count += ports_per_process
         else:
-            match ports_to_scan:
-                case 0:
-                    ports_per_process = int(len(self.SML_LIST)/self.MAX_PROCESSES)
-                    for process_id in range(1, self.MAX_PROCESSES):
-                        process = Process.run(target=managed_scan_port, args=(target_ip, False, '0', self.SML_LIST[last_ports_count:(last_ports_count + ports_per_process)], return_dictionary, process_id))
-                        scan_processes.append(process)
-                case 1:
-                    ports_per_process = int(len(self.MED_LIST)/self.MAX_PROCESSES)
-                    for process_id in range(1, self.MAX_PROCESSES):
-                        process = Process.run(target=managed_scan_port, args=(target_ip, False, '0', self.MED_LIST[last_ports_count:(last_ports_count + ports_per_process)], return_dictionary, process_id))
-                        scan_processes.append(process)
-                case _:
-                    raise PortScannerError(f"Incompatable input scan type \'{ports_to_scan}\'!")
-            for process in scan_processes:
-                process.join()
-            for key in scan_processes.keys():
-                for port in scan_processes[key]:
-                    open_ports.append(port)
+            if syn_scan is True:
+                match ports_to_scan:
+                    case 0:
+                        ports_per_process = int(len(self.SML_LIST)/self.MAX_PROCESSES)
+                        for process_id in range(1, self.MAX_PROCESSES):
+                            process = Process(target=PortScanner().managed_scan_port, args=(target_ip, 'S', PortScanner().SML_LIST[last_ports_count:(last_ports_count + ports_per_process)], return_dictionary, process_id))
+                            process.start()
+                            scan_processes.append(process)
+                            last_ports_count += ports_per_process
+                    case 1:
+                        ports_per_process = int(len(self.MED_LIST)/self.MAX_PROCESSES)
+                        for process_id in range(1, self.MAX_PROCESSES):
+                            process = Process(target=PortScanner().managed_scan_port, args=(target_ip, 'S', PortScanner().MED_LIST[last_ports_count:(last_ports_count + ports_per_process)], return_dictionary, process_id))
+                            process.start()
+                            scan_processes.append(process)
+                            last_ports_count += ports_per_process
+                    case _:
+                        raise PortScannerError(f"Incompatable input scan type \'{ports_to_scan}\'!")
+            elif syn_scan is False:
+                match ports_to_scan:
+                    case 0:
+                        ports_per_process = int(len(self.SML_LIST)/self.MAX_PROCESSES)
+                        for process_id in range(1, self.MAX_PROCESSES):
+                            process = Process(target=PortScanner().managed_scan_port, args=(target_ip, 'A', PortScanner().SML_LIST[last_ports_count:(last_ports_count + ports_per_process)], return_dictionary, process_id))
+                            process.start()
+                            scan_processes.append(process)
+                            last_ports_count += ports_per_process
+                    case 1:
+                        ports_per_process = int(len(self.MED_LIST)/self.MAX_PROCESSES)
+                        for process_id in range(1, self.MAX_PROCESSES):
+                            process = Process(target=PortScanner().managed_scan_port, args=(target_ip, 'A', PortScanner().MED_LIST[last_ports_count:(last_ports_count + ports_per_process)], return_dictionary, process_id))
+                            process.start()
+                            scan_processes.append(process)
+                            last_ports_count += ports_per_process
+                    case _:
+                        raise PortScannerError(f"Incompatable input scan type \'{ports_to_scan}\'!")
+            else:
+                raise PortScannerError("Incompatable syn_scan type!")
+        for process in scan_processes:
+            process.join()
+        for key in return_dictionary.keys():
+            for port in return_dictionary[key]:
+                open_ports.append(port)
         return open_ports
